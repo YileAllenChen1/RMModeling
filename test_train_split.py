67bc4a7046e5b6864d9571875342570172d53f5b
1YileChen <yl2015000102@gmail.com>
2020-09-19 10:25:38 -0500

    modified test train split

--- test_train_split.py
+++ test_train_split.py
@@ -3,9 +3,9 @@ import os
 import random
 
 
-source = r"C:/Users/11458/Desktop/RMModeling/renamed_images"
-destination_train = r"C:/Users/11458/Desktop/RMModeling/train"
-destination_test = r"C:/Users/11458/Desktop/RMModeling/test"
+source = r"C:/Users/11458/Desktop/RMModeling/images/renamed_images"
+destination_train = r"C:/Users/11458/Desktop/RMModeling/images/train"
+destination_test = r"C:/Users/11458/Desktop/RMModeling/images/test"
 
 files = os.listdir(source)	#get all the files in directory, stored in list
 
@@ -13,9 +13,12 @@ test_train_ratio = 0.2	#ratio of test samples from total
 test_images = random.sample(files, int(len(files)*test_train_ratio))
 train_images = list(set(files) - set(test_images))
 
-for test_image in test_images:
-	new_test_path = shutil.move(f"{source}/{test_image}", destination_test)
+for count, test_image in enumerate(test_images):
+	if test_images not in os.listdir(destination_test):
+		new_test_path = shutil.copy(f"{source}/{test_image}", destination_test)
+	print(count)
 
-
-for train_image in train_images:
-	new_train_path = shutil.move(f"{source}/{train_image}", destination_train)
+for count, train_image in enumerate(train_images):
+	if train_images not in os.listdir(destination_train):
+		new_train_path = shutil.copy(f"{source}/{train_image}", destination_train)
+	print(count)
