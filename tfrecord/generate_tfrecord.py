4bc32c88e83e12a13dc49a2716dd4c884f1dad5b
1YileChen <yl2015000102@gmail.com>
2020-10-23 23:15:50 -0500

    Update generate_tfrecord.py

--- tfrecord/generate_tfrecord.py
+++ tfrecord/generate_tfrecord.py
@@ -14,7 +14,7 @@ from __future__ import absolute_import
 import os
 import io
 import pandas as pd
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 
 from PIL import Image
 from object_detection.utils import dataset_util
